bcb010446a88007fd55484ae02464ee4f7e542af
NguyenQuocVuong172051 <112994006+NguyenQuocVuong172051@users.noreply.github.com>
2022-09-17 09:05:53 +0700

    Add files via upload

---
+++ Bai tap ve nha- Tao bang.cpp
@@ -0,0 +1,132 @@
+//Preprocessor Directives
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main ()
+{
+	int i ;
+	
+	printf("%c",218);
+	for (i=0; i<9; i++)
+	printf("%c",196);
+	printf("%c",194);
+	for (i=0; i<9; i++)
+	printf("%c",196);
+	printf("%c", 191);
+	printf("\n");
+	
+	printf("%c", 179);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%d", 1);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%c", 179);
+	for (i = 0; i<1; i++)
+	printf(" ");
+	printf("HE172051");
+	printf("%c", 179);
+	printf("\n");
+	
+	printf("%c", 195);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",197);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",180);
+	printf("\n");
+	
+	printf("%c", 179);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%d", 2);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%c", 179);
+	for (i = 0; i<1; i++)
+	printf(" ");
+	printf("HE172052");
+	printf("%c", 179);
+	printf("\n");
+	
+	printf("%c", 195);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",197);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",180);
+	printf("\n");
+	
+	printf("%c", 179);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%d", 3);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%c", 179);
+	for (i = 0; i<1; i++)
+	printf(" ");
+	printf("HE172053");
+	printf("%c", 179);
+	printf("\n");
+	
+	printf("%c", 195);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",197);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",180);
+	printf("\n");
+	
+	printf("%c", 179);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%d", 4);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%c", 179);
+	for (i = 0; i<1; i++)
+	printf(" ");
+	printf("HE172054");
+	printf("%c", 179);
+	printf("\n");
+	
+	printf("%c", 195);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",197);
+	for (i = 0; i<9; i++)
+	printf("%c",196);
+	printf("%c",180);
+	printf("\n");
+	
+	printf("%c", 179);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%d", 5);
+	for (i = 0; i<4; i++)
+	printf(" ");
+	printf("%c", 179);
+	for (i = 0; i<1; i++)
+	printf(" ");
+	printf("HE172055");
+	printf("%c", 179);
+	printf("\n");
+	
+	printf("%c", 192);
+	for (i=0; i<9; i++)
+	printf("%c",196);
+	printf("%c",193);
+	for (i=0; i<9; i++)
+	printf("%c",196);
+	printf("%c",217);
+	
+	
+	
+	return 0;
+}
